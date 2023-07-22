755ea1236f04d7dfe4e86c8d9a0ea10caff02adc
Tong Tang <pichip22@hotmail.com>
2023-07-22 07:59:57 -0400

    completed

--- lib/song.py
+++ lib/song.py
@@ -1,2 +1,40 @@
 class Song:
-    pass
+    count = 0
+    names = []
+    artists = []
+    genres = []
+    genre_count = {}
+    artist_count = {}
+
+    def __init__(self, name, artist, genre):
+        self.name = name
+        self.artist = artist
+        self.genre = genre
+        Song.count += 1
+        Song.add_to_genres(genre)
+        Song.add_to_artist(artist)
+        Song.add_genre_count(genre)
+        Song.add_artist_count(artist)
+    @classmethod
+    def add_to_genres(cls, genre):
+        if genre not in cls.genres:
+            cls.genres.append(genre)
+            
+    @classmethod
+    def add_to_artist(cls, artist):
+        if artist not in cls.artists:
+            cls.artists.append(artist)
+
+    @classmethod
+    def add_genre_count(cls, genre):
+        if genre not in cls.genre_count:
+            cls.genre_count[genre] = 1
+        else:
+            cls.genre_count[genre] += 1
+    
+    @classmethod
+    def add_artist_count(cls, artist):
+        if artist not in cls.artist_count:
+            cls.artist_count[artist] = 1
+        else:
+            cls.artist_count[artist] += 1
